87ae08202806fa6913c48fe3dbb11997e5b34db0
Fabian Colque <fbcolque@gmail.com>
2021-09-22 23:13:45 -0300

    Ajuste de 3products sql

--- db-script/3_products1.sql
+++ db-script/3_products1.sql
@@ -1,77 +1,80 @@
-('Chai', '10 boxes x 30 bags','1',8,18,0.1,1,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Chang', '24 - 12 oz bottles','2',1,19,0.5,1,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Aniseed Syrup', '12 - 550 ml bottles','3',1,10,0.6,2,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Chef Anton''s Cajun Seasoning', '48 - 6 oz jars','4',2,22,0.7,2,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Chef Anton''s Gumbo Mix', '36 boxes','5',2, 21.3500004,0.8,2,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Grandma''s Boysenberry Spread', '12 - 8 oz jars','6',3,25,0.9,2,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Uncle Bob''s Organic Dried Pears', '12 - 1 lb pkgs.','7',3,30,0.10,7,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Northwoods Cranberry Sauce', '12 - 12 oz jars','8',3,40,0.11,2,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Mishi Kobe Niku', '18 - 500 g pkgs.','9',4,97,0.12,6,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Ikura', '12 - 200 ml jars','10',4,31,0.13,8,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Queso Cabrales', '1 kg pkg.','11',5,21,0.14,4,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Queso Manchego La Pastora', '10 - 500 g pkgs.','12',5,38,0.15,4,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Konbu', '2 kg box','13',6,6,0.16,8,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Tofu', '40 - 100 g pkgs.','14',6, 23.25,0.17,7,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Genen Shouyu', '24 - 250 ml bottles','15',6,13,0.18,2,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Pavlova', '32 - 500 g boxes','16',7, 17.4500008,0.19,3,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Alice Mutton', '20 - 1 kg tins','17',7,39,0.20,6,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Carnarvon Tigers', '16 kg pkg.','18',7, 62.5,0.21,8,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Teatime Chocolate Biscuits', '10 boxes x 12 pieces','19',8, 9.19999981,0.22,3,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Sir Rodney''s Marmalade', '30 gift boxes','20',8,81,0.23,3,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Sir Rodney''s Scones', '24 pkgs. x 4 pieces','21',8,10,0.24,3,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Gustaf''s Knäckebröd', '24 - 500 g pkgs.','22',9,21,0.25,5,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Tunnbröd', '12 - 250 g pkgs.','23',9,9,0.26,5,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Guaraná Fantástica', '12 - 355 ml cans','24',10, 4.5,0.27,1,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('NuNuCa Nuß-Nougat-Creme', '20 - 450 g glasses','25',11,14,0.28,3,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Gumbär Gummibärchen', '100 - 250 g bags','26',11, 31.2299995,0.29,3,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Schoggi Schokolade', '100 - 100 g pieces','27',11, 43.9000015,0.30,3,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Rössle Sauerkraut', '25 - 825 g cans','28',12, 45.5999985,0.31,7,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Thüringer Rostbratwurst', '50 bags x 30 sausgs.','29',12, 123.790001,0.32,6,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Nord-Ost Matjeshering', '10 - 200 g glasses','30',13, 25.8899994,0.33,8,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Gorgonzola Telino', '12 - 100 g pkgs','31',14, 12.5,0.34,4,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Mascarpone Fabioli', '24 - 200 g pkgs.','32',14,32,0.35,4,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Geitost', '500 g','33',15, 2.5,0.36,4,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Sasquatch Ale', '24 - 12 oz bottles','34',16,14,0.37,1,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Steeleye Stout', '24 - 12 oz bottles','35',16,18,0.38,1,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Inlagd Sill', '24 - 250 g  jars','36',17,19,0.39,8,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Gravad lax', '12 - 500 g pkgs.','37',17,26,0.40,8,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Côte de Blaye', '12 - 75 cl bottles','38',18, 263.5,0.41,1,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Chartreuse verte', '750 cc per bottle','39',18,18,0.42,1,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Boston Crab Meat', '24 - 4 oz tins','40',19, 18.3999996,0.43,8,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Jack''s New England Clam Chowder', '12 - 12 oz cans','41',19, 9.64999962,0.44,8,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Singaporean Hokkien Fried Mee', '32 - 1 kg pkgs.','42',20,14,0.45,5,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Ipoh Coffee', '16 - 500 g tins','43',20,46,0.46,1,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Gula Malacca', '20 - 2 kg bags','44',20, 19.4500008,0.47,2,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Rogede sild', '1k pkg.','45',21, 9.5,0.48,8,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Spegesild', '4 - 450 g glasses','46',21,12,0.49,8,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Zaanse koeken', '10 - 4 oz boxes','47',22, 9.5,0.50,3,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Chocolade', '10 pkgs.','48',22, 12.75,0.51,3,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Maxilaku', '24 - 50 g pkgs.','49',23,20,0.52,3,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Valkoinen suklaa', '12 - 100 g bars','50',23, 16.25,0.53,3,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Manjimup Dried Apples', '50 - 300 g pkgs.','51',24,53,0.54,7,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Filo Mix', '16 - 2 kg boxes','52',24,7,0.55,5,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Perth Pasties', '48 pieces','53',24, 32.7999992,0.56,6,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Tourtière', '16 pies','54',25, 7.44999981,0.57,6,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Pâté chinois', '24 boxes x 2 pies','55',25,24,0.58,6,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Gnocchi di nonna Alice', '24 - 250 g pkgs.','56',26,38,0.59,5,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Ravioli Angelo', '24 - 250 g pkgs.','57',26, 19.5,0.60,5,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Escargots de Bourgogne', '24 pieces','58',27, 13.25,0.61,8,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Raclette Courdavault', '5 kg pkg.','59',28,55,0.62,4,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Camembert Pierrot', '15 - 300 g rounds','60',28,34,0.63,4,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Sirop d''érable', '24 - 500 ml bottles','61',29, 28.5,0.64,2,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Tarte au sucre', '48 pies','62',29, 49.2999992,0.65,3,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Vegie-spread', '15 - 625 g jars','63',7, 43.9000015,0.66,2,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Wimmers gute Semmelknödel', '20 bags x 4 pieces','64',12, 33.25,0.67,5,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Louisiana Fiery Hot Pepper Sauce', '32 - 8 oz bottles','65',2, 21.0499992,0.68,2,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Louisiana Hot Spiced Okra', '24 - 8 oz jars','66',2,17,0.69,2,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Laughing Lumberjack Lager', '24 - 12 oz bottles','67',16,14,0.70,1,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Scottish Longbreads', '10 boxes x 8 pieces','68',8, 12.5,0.71,3,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Gudbrandsdalsost', '10 kg pkg.','69',15,36,0.72,4,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Outback Lager', '24 - 355 ml bottles','70',7,15,0.73,1,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Flotemysost', '10 - 500 g pkgs.','71',15, 21.5,0.74,4,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Mozzarella di Giovanni', '24 - 200 g pkgs.','72',14, 34.7999992,0.75,4,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Röd Kaviar', '24 - 150 g jars','73',17,15,0.76,8,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Longlife Tofu', '5 kg pkg.','74',4,10,0.77,7,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Rhönbräu Klosterbier', '24 - 0.5 l bottles','75',12, 7.75,0.78,1,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Lakkalikööri', '500 ml','76',23,18,0.79,1,'A','2021-01-01 00:00:00',0,null,null,false,0)
-('Original Frankfurter grüne Soße', '12 boxes','77',12,13,0.80,2,'A','2021-01-01 00:00:00',0,null,null,false,0)
+insert into se_product
+( v_name, v_description, v_serial_number, i_quantity, d_price, d_weight, d_volume, c_state, t_create_date, i_create_id, t_update_date, i_update_id, b_deleted, i_organization_id)
+values
+('Chai', '10 boxes x 30 bags','1',8,18,0.1,1,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Chang', '24 - 12 oz bottles','2',1,19,0.5,1,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Aniseed Syrup', '12 - 550 ml bottles','3',1,10,0.6,2,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Chef Anton''s Cajun Seasoning', '48 - 6 oz jars','4',2,22,0.7,2,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Chef Anton''s Gumbo Mix', '36 boxes','5',2, 21.3500004,0.8,2,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Grandma''s Boysenberry Spread', '12 - 8 oz jars','6',3,25,0.9,2,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Uncle Bob''s Organic Dried Pears', '12 - 1 lb pkgs.','7',3,30,0.10,7,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Northwoods Cranberry Sauce', '12 - 12 oz jars','8',3,40,0.11,2,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Mishi Kobe Niku', '18 - 500 g pkgs.','9',4,97,0.12,6,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Ikura', '12 - 200 ml jars','10',4,31,0.13,8,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Queso Cabrales', '1 kg pkg.','11',5,21,0.14,4,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Queso Manchego La Pastora', '10 - 500 g pkgs.','12',5,38,0.15,4,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Konbu', '2 kg box','13',6,6,0.16,8,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Tofu', '40 - 100 g pkgs.','14',6, 23.25,0.17,7,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Genen Shouyu', '24 - 250 ml bottles','15',6,13,0.18,2,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Pavlova', '32 - 500 g boxes','16',7, 17.4500008,0.19,3,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Alice Mutton', '20 - 1 kg tins','17',7,39,0.20,6,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Carnarvon Tigers', '16 kg pkg.','18',7, 62.5,0.21,8,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Teatime Chocolate Biscuits', '10 boxes x 12 pieces','19',8, 9.19999981,0.22,3,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Sir Rodney''s Marmalade', '30 gift boxes','20',8,81,0.23,3,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Sir Rodney''s Scones', '24 pkgs. x 4 pieces','21',8,10,0.24,3,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Gustaf''s Knäckebröd', '24 - 500 g pkgs.','22',9,21,0.25,5,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Tunnbröd', '12 - 250 g pkgs.','23',9,9,0.26,5,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Guaraná Fantástica', '12 - 355 ml cans','24',10, 4.5,0.27,1,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('NuNuCa Nuß-Nougat-Creme', '20 - 450 g glasses','25',11,14,0.28,3,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Gumbär Gummibärchen', '100 - 250 g bags','26',11, 31.2299995,0.29,3,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Schoggi Schokolade', '100 - 100 g pieces','27',11, 43.9000015,0.30,3,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Rössle Sauerkraut', '25 - 825 g cans','28',12, 45.5999985,0.31,7,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Thüringer Rostbratwurst', '50 bags x 30 sausgs.','29',12, 123.790001,0.32,6,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Nord-Ost Matjeshering', '10 - 200 g glasses','30',13, 25.8899994,0.33,8,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Gorgonzola Telino', '12 - 100 g pkgs','31',14, 12.5,0.34,4,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Mascarpone Fabioli', '24 - 200 g pkgs.','32',14,32,0.35,4,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Geitost', '500 g','33',15, 2.5,0.36,4,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Sasquatch Ale', '24 - 12 oz bottles','34',16,14,0.37,1,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Steeleye Stout', '24 - 12 oz bottles','35',16,18,0.38,1,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Inlagd Sill', '24 - 250 g  jars','36',17,19,0.39,8,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Gravad lax', '12 - 500 g pkgs.','37',17,26,0.40,8,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Côte de Blaye', '12 - 75 cl bottles','38',18, 263.5,0.41,1,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Chartreuse verte', '750 cc per bottle','39',18,18,0.42,1,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Boston Crab Meat', '24 - 4 oz tins','40',19, 18.3999996,0.43,8,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Jack''s New England Clam Chowder', '12 - 12 oz cans','41',19, 9.64999962,0.44,8,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Singaporean Hokkien Fried Mee', '32 - 1 kg pkgs.','42',20,14,0.45,5,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Ipoh Coffee', '16 - 500 g tins','43',20,46,0.46,1,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Gula Malacca', '20 - 2 kg bags','44',20, 19.4500008,0.47,2,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Rogede sild', '1k pkg.','45',21, 9.5,0.48,8,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Spegesild', '4 - 450 g glasses','46',21,12,0.49,8,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Zaanse koeken', '10 - 4 oz boxes','47',22, 9.5,0.50,3,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Chocolade', '10 pkgs.','48',22, 12.75,0.51,3,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Maxilaku', '24 - 50 g pkgs.','49',23,20,0.52,3,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Valkoinen suklaa', '12 - 100 g bars','50',23, 16.25,0.53,3,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Manjimup Dried Apples', '50 - 300 g pkgs.','51',24,53,0.54,7,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Filo Mix', '16 - 2 kg boxes','52',24,7,0.55,5,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Perth Pasties', '48 pieces','53',24, 32.7999992,0.56,6,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Tourtière', '16 pies','54',25, 7.44999981,0.57,6,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Pâté chinois', '24 boxes x 2 pies','55',25,24,0.58,6,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Gnocchi di nonna Alice', '24 - 250 g pkgs.','56',26,38,0.59,5,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Ravioli Angelo', '24 - 250 g pkgs.','57',26, 19.5,0.60,5,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Escargots de Bourgogne', '24 pieces','58',27, 13.25,0.61,8,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Raclette Courdavault', '5 kg pkg.','59',28,55,0.62,4,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Camembert Pierrot', '15 - 300 g rounds','60',28,34,0.63,4,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Sirop d''érable', '24 - 500 ml bottles','61',29, 28.5,0.64,2,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Tarte au sucre', '48 pies','62',29, 49.2999992,0.65,3,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Vegie-spread', '15 - 625 g jars','63',7, 43.9000015,0.66,2,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Wimmers gute Semmelknödel', '20 bags x 4 pieces','64',12, 33.25,0.67,5,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Louisiana Fiery Hot Pepper Sauce', '32 - 8 oz bottles','65',2, 21.0499992,0.68,2,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Louisiana Hot Spiced Okra', '24 - 8 oz jars','66',2,17,0.69,2,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Laughing Lumberjack Lager', '24 - 12 oz bottles','67',16,14,0.70,1,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Scottish Longbreads', '10 boxes x 8 pieces','68',8, 12.5,0.71,3,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Gudbrandsdalsost', '10 kg pkg.','69',15,36,0.72,4,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Outback Lager', '24 - 355 ml bottles','70',7,15,0.73,1,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Flotemysost', '10 - 500 g pkgs.','71',15, 21.5,0.74,4,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Mozzarella di Giovanni', '24 - 200 g pkgs.','72',14, 34.7999992,0.75,4,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Röd Kaviar', '24 - 150 g jars','73',17,15,0.76,8,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Longlife Tofu', '5 kg pkg.','74',4,10,0.77,7,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Rhönbräu Klosterbier', '24 - 0.5 l bottles','75',12, 7.75,0.78,1,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Lakkalikööri', '500 ml','76',23,18,0.79,1,'A','2021-01-01 00:00:00',0,null,null,false,0),
+('Original Frankfurter grüne Soße', '12 boxes','77',12,13,0.80,2,'A','2021-01-01 00:00:00',0,null,null,false,0);
